5460903300f279658fe9e35f3461927b5be7557f
tmors <398080994@163.com>
2017-02-15 14:58:07 +0800

    checkout for remote

--- wiki_word2vec/separate_words.py
+++ wiki_word2vec/separate_words.py
@@ -1,8 +1,9 @@
 __author__ = 'huang'
 
-import os
 import logging
+import os
 import sys
+
 import jieba
 
 if __name__=='__main__':
@@ -21,9 +22,15 @@ if __name__=='__main__':
     space = ' '
 
     output = open(outp, 'w')
-    inp = open('wiki.cn.text.jian', 'r')
-
+    # inp = open('wiki.cn.text.jian', 'r')
+    inp = open(inp, 'r')
+    i = 1
     for line in inp.readlines():
+        if line == '\n' or line == "":
+            continue
         seg_list = jieba.cut(line)
-        output.write(space.join(seg_list) + '\n')
-
+        str = space.join(seg_list)
+        output.write(str)
+        if i % 1000 == 0:
+            print(i, "finished")
+    output.close()
