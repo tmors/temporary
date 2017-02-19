d74d65500b10241fc35713a595d8948c943c1de8
root <root@ubuntu-512mb-nyc2-01.localdomain>
2017-02-19 13:48:56 +0000

    modify

--- test.py
+++ test.py
@@ -1,28 +1,31 @@
 #coding="utf-8"
 from lsaProcess import calcWordWeight
 from numpy import *
-if "__name__" == "__main__":
-	matrixFilePath = "/usr/wikiDataSet/word_document.matrix"
-	matrixFile = open(matrixFilePath,"r")
-	wdMatrixList = []
-	curReadLine = 0
-	while(matrixFile.readable()):
-		curLine = matrixFile.readline()
-		curReadLine += 1
-		print(curReadLine,"curLine read")
-		if(curLine==""):
-			break
-		curLineList = curLine.split(" ")
-		wdMatrixList.append(curLineList)
-	wdMatrix = mat(wdMatrixList)		
-	wordWeight = calcWordWeight(wdMatrix)
-	curWordWeight = 0
-	outputFilePath = "/usr/wikiDataSet/wordWeight.matrix"
-	outputFile = open(outputFilePath,"w")
-	for i in wordWeight:
-		curWordWeight += 1
-		outputFile.write(i + "\n")
-		print(curWordWeight,"finished")
-	outputFile.close()
-		
-		
+
+def buildWDMatrixByList(listPath):
+    matrixFilePath = listPath
+    matrixFile = open(matrixFilePath,"r")
+    wdMatrixList = []
+    curReadLine = 0
+    lineLimitation = 3000
+    while(curReadLine < lineLimitation):
+        curLine = matrixFile.readline()
+        curReadLine += 1
+        print(curReadLine,"curLine read")
+        if(curLine==""):
+            break
+        curLineList = curLine.split(" ")
+        wdMatrixList.append(curLineList)
+    wdMatrix = mat(wdMatrixList)
+    return wdMatrixList
+if __name__ == "__main__":
+    wdMatrixList = buildWDMatrixByList("/usr/wikiDataSet/word_document.matrix")
+    wordWeight = calcWordWeight(wdMatrixList)
+    curWordWeight = 0
+    outputFilePath = "/usr/wikiDataSet/wordWeight.matrix"
+    outputFile = open(outputFilePath,"w")
+    for i in wordWeight:
+        outputFile.write(str(curWordWeight) + " " + str(i) + "\n")
+        curWordWeight += 1
+        print(curWordWeight,"finished")
+    outputFile.close()
